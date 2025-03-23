b94882f0bab48e133f18c5a23d2cae5d86e50291
aanchik <annasemryagina@gmail.com>
2025-03-23 22:56:08 +0300

    добавлены комментарии
    доделать train, train2, processing_data

--- metric.py
+++ metric.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-def decision_prices(test):
-    test = test.set_index('dt') 
+def decision_prices(test):  #функция принимает на вход test с данными по арматуре
+    test = test.set_index('dt')  #dt становится индексом
     tender_price = test['Цена на арматуру']
-    decision = test['Объем']
-    start_date = test.index.min()
+    decision = test['Объем']  #колонка с количеством недель закупки
+    start_date = test.index.min() #определение границ временного диапазона
     end_date = test.index.max()
     
-    _results = []
+    _results = []  #список для хранения цен, по которым закупалась арматура
     _active_weeks = 0
-    for report_date in pd.date_range(start_date, end_date, freq='W-MON'):
+    for report_date in pd.date_range(start_date, end_date, freq='W-MON'):  #проходим по всем понедельникам в диапазоне start_date — end_date.
         if _active_weeks == 0:  # Пришла пора нового тендера
-            _fixed_price = tender_price.loc[report_date]
+            _fixed_price = tender_price.loc[report_date]  #запоминает цену на арматуру
             _active_weeks = int(decision.loc[report_date])
         _results.append(_fixed_price)
         _active_weeks += -1
-    cost = sum(_results)
-    return cost # Возвращаем затраты на периоде
+    cost = sum(_results)  #суммирует все зафиксированные цены
+    return cost #dозвращаем итоговые затраты на арматуру
